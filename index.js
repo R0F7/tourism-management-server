9a82fbcd479be66f3ab0549b9135ff9add47d567
R0F7 <wwwrafikhan075@gmail.com>
2024-04-27 18:04:26 +0600

    added single read operation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +31,13 @@ async function run() {
     app.get('/tourists-spot', async (req, res) => {
         const cursor = tourismCollection.find();
         const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.get('/tourists-spot/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await tourismCollection.findOne(query);
         res.send(result)
     })
 
